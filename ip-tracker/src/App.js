ac018ee07418ad157f6790bb299104d25aa2d3c9
Alyssa Hooper <nesteralyssa@gmail.com>
2021-01-28 12:00:49 -0800

    Inital Config

---
+++ ip-tracker/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+
+      IP Address Tracker
+
+      Search for any IP address or domain
+
+      IP Address
+      Location
+      Timezone
+        UTC /* add offset value dynamically using the API*/
+      ISP
+
+    </div>
+  );
+}
+
+export default App;
