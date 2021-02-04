2a6508083bb05c4bbc51d4df54768c0119dec296
Alyssa Hooper <nesteralyssa@gmail.com>
2021-02-04 10:31:45 -0800

    Renders JSON

--- ip-tracker/src/components/IPLocation.js
+++ ip-tracker/src/components/IPLocation.js
@@ -2,36 +2,20 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function IPLocation() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get("https://vpn-proxy-detection.ipify.org/api/v1?apiKey=at_hyVlu8fTCFs7utA721MugqjzgxgqH&ipAddress=8.8.8.8&")
-      .then(res =>
-        console.log(res))
-      .then(
-        (res) => {
-          setData(res);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
+    axios
+      .get("https://vpn-proxy-detection.ipify.org/api/v1?apiKey=at_hyVlu8fTCFs7utA721MugqjzgxgqH&ipAddress=8.8.8.8&")
+      .then(response => setData(response.data));
+  }, []);
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <ul>
-        {data.map((data) => data.data.ip)}
-      </ul>
-    );
-  }
+  return (
+    <div>
+      {JSON.stringify(data)}
+    </div>
+  );
+
 }
 
 export default IPLocation
